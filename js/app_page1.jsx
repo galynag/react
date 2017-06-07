6d302cf5a782fe6915838a3b7622700f9c44c09e
galynag <g.alenicheva@gmail.com>
2017-06-07 23:52:09 +0300

    v0.5 app1 was added

--- js/app_page1.jsx
+++ js/app_page1.jsx
@@ -1,34 +1,22 @@
-var btnMass={
-    '1':'Home',
-    '2':'Books',
-    '3':'Contacts'
-};
+
 var Btn = React.createClass({
     render : function() {
-        var dataM = this.props.dataMass;
-        var libraryTemplates;
-        if (dataM.length >0) {
-            libraryTemplates = data.map(function(item, index) {
-                return (
-                    <div className="infoBook" key={index}>
-                        <Element dataElement={item}/>
+         return (
+                    <div>
+                        <div className="waves-effect waves-light btn">Home</div>
+                        <div className="waves-effect waves-light btn">Books</div>
+                        <div className="waves-effect waves-light btn">Contacts</div>
+                        <input type="search" placeholder="Search" />
+                        <a id="menu" className="waves-effect waves-light btn btn-floating" onclick="$('.tap-target').tapTarget('open')"><i className="material-icons">menu</i></a>
+                        <div className="tap-target" data-activates="menu">
+                            <div className="tap-target-content">
+                                <h5>Title</h5>
+                                <p>A bunch of text</p>
+                            </div>
+                        </div>
                     </div>
-                )
-            })
-        } else {
-            libraryTemplates = <p className="subtitle">В библиотеке нет ни одной книги</p>
-        }
-        return (
-            <div>
-                {libraryTemplates}
-                <p
-                    onClick={this.counterClick}
-                    className={(data.length > 0) ? 'infoBook subtitle':'hide'}
-                >
-                    Количество книг в библиотеке: {data.length}
-                </p>
-            </div>
-        )
+         )
+
     }
 })
 
@@ -36,8 +24,8 @@ var Container1 = React.createClass({
     render: function() {
         return (
             <div>
-                <Btn dataMass={btnMass}/>
-                <BtnFooter />
+                <Btn />
+
             </div>
         )
     }
